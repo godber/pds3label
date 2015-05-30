5301a73bcad3e2ba5e87abc1f42b51adced4a84c
Austin Godber <godber@uberhip.com>
2015-05-30 11:06:34 -0700

    added test for simple_image_1

---
+++ test/test_pds3label_simple_image_1.py
@@ -0,0 +1,26 @@
+"""
+Tests for `pds3label` module.
+"""
+import pytest
+from pds3label import Pds3Label
+
+def test_based_integer():
+    f = 'test/data/simple_image_1.lbl'
+    label = Pds3Label(f)
+    assert label.infile == f
+    print label
+    assert label['RECORD_TYPE'] == 'FIXED_LENGTH'
+    assert label['RECORD_BYTES'] == 824
+    assert label['LABEL_RECORDS'] == 1
+    assert label['FILE_RECORDS'] == 601
+    assert label['IMAGE']['LINES'] == 600
+    assert label['IMAGE']['LINE_SAMPLES'] == 824
+    image_group = label['IMAGE']
+    assert image_group['SAMPLE_TYPE'] == 'MSB_INTEGER'
+    assert image_group['SAMPLE_BITS'] == 8
+    assert abs(image_group['MEAN'] - 51.6778539644) <= 0.00001
+    assert image_group['MEDIAN'] == 50.0
+    assert image_group['MINIMUM'] == 0
+    assert image_group['MAXIMUM'] == 255
+    assert image_group['STANDARD_DEVIATION'] == 16.97019
+    assert image_group['CHECKSUM'] == 25549531
